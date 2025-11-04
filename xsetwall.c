b2a5d7d47e20924073033a6fb0d7f4c5d08df6b9
leanghok120 <leanghokoeng5@gmail.com>
2025-11-04 10:02:32 +0700

    Revert "allow setting solid color wallpaper or image wallpaper"
    This reverts commit ded3bd62c2923be6776ed2af4c24ef96df18be65.

--- xsetwall.c
+++ xsetwall.c
@@ -3,30 +3,19 @@
 #include <Imlib2.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-Display *dpy;
-Window root;
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("usage: %s path\n", argv[0]);
+    return 1;
+  }
 
-void print_help(const char *name) {
-  printf("usage: %s [-c 0xRRGGBB | -p /path/to/image]\n", name);
-}
-
-void die(const char *msg) {
-  fprintf(stderr, "%s\n", msg); 
-  exit(1);
-}
-
-void setbgcolor(unsigned long color) {
-  XSetWindowBackground(dpy, root, color);
-  XClearWindow(dpy, root);
-}
-
-void setbgimage(const char *path) {
+  // setup xlib
+  Display *dpy = XOpenDisplay(NULL);
+  Window root = XDefaultRootWindow(dpy);
   int scr = DefaultScreen(dpy);
   int width = DisplayWidth(dpy, scr);
   int height = DisplayHeight(dpy, scr);
-
   Pixmap pixmap = XCreatePixmap(dpy, root, width, height, DefaultDepth(dpy, scr));
 
   // setup imlib
@@ -37,9 +26,10 @@ void setbgimage(const char *path) {
 
   // load image
   Imlib_Image image;
-  image = imlib_load_image(path);
+  image = imlib_load_image(argv[1]);
   if (image == NULL) {
-    die("failed to load image");
+    fprintf(stderr, "failed to load image\n"); 
+    exit(1);
   }
   imlib_context_set_image(image);
   imlib_render_image_on_drawable_at_size(0, 0, width, height);
@@ -47,31 +37,10 @@ void setbgimage(const char *path) {
   // set bg
   XSetWindowBackgroundPixmap(dpy, root, pixmap);
   XClearWindow(dpy, root);
+  XFlush(dpy);
 
   imlib_free_image();
   XFreePixmap(dpy, pixmap);
-}
-
-int main(int argc, char **argv) {
-  if (argc < 2) {
-    print_help(argv[0]);
-    return 1;
-  }
-
-  dpy = XOpenDisplay(NULL);
-  root = XDefaultRootWindow(dpy);
-
-  if (strcmp(argv[1], "-p") == 0) {
-    setbgimage(argv[2]);
-  } else if (strcmp(argv[1], "-c") == 0) {
-    unsigned long color = strtoul(argv[2], NULL, 0);
-    setbgcolor(color);
-  } else {
-    print_help(argv[0]);
-  }
-
-  XFlush(dpy);
-
   XCloseDisplay(dpy);
   return 0;
 }
